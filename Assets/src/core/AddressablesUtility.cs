b8667e598615704a45bbcca07909f89efb786ab2
thz <lixuepeng@bilibili.com>
2022-04-19 01:34:04 +0800

    [通过addressable 加载脚本]

---
+++ Assets/src/core/AddressablesUtility.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+/// <summary>
+/// A utility class for various Addressables functionality
+/// </summary>
+public static class AddressablesUtility
+{
+    /// <summary>
+    /// Get the address of a given AssetReference.
+    /// </summary>
+    /// <param name="reference">The AssetReference you want to find the address of.</param>
+    /// <returns>The address of a given AssetReference.</returns>
+    public static string GetAddressFromAssetReference(AssetReference reference)
+    {
+        var loadResourceLocations = Addressables.LoadResourceLocationsAsync(reference);
+        var result = loadResourceLocations.WaitForCompletion();
+        if (result.Count > 0)
+        {
+            string key = result[0].PrimaryKey;
+            Addressables.Release(loadResourceLocations);
+            return key;
+        }
+
+        Addressables.Release(loadResourceLocations);
+        return string.Empty;
+    }
+
+    public static byte[] GetBytesFromAssetReference(AssetReference reference)
+    {
+        var handle = reference.LoadAssetAsync<TextAsset>();
+        var result = handle.WaitForCompletion();
+        return result.bytes;
+    }
+   
+    public static byte[] GetBytesFromAssetPath(string path)
+    {
+        var handle = Addressables.LoadAssetAsync<TextAsset>(path);
+        var result = handle.WaitForCompletion();
+        Addressables.Release(handle);
+        return result.bytes;
+    }
+}
+
--- Assets/src/core/LuaState.cs
+++ Assets/src/core/LuaState.cs
@@ -2,13 +2,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AddressableAssets;
 using XLua;
 
 public class LuaState : MonoBehaviour
 {
-    public LuaAsset luaScript;
-    // public Injection[] injections;
-
     internal static LuaEnv luaEnv = new LuaEnv(); //all lua behaviour shared one luaenv only!
     internal static float lastGCTime = 0;
     internal const float GCInterval = 1;//1 second 
@@ -22,7 +20,10 @@ public class LuaState : MonoBehaviour
     void Awake()
     {
         scriptEnv = luaEnv.NewTable();
-
+        luaEnv.AddLoader((ref string filepath) =>
+        {
+            return AddressablesUtility.GetBytesFromAssetPath($"Assets/res/scripts/{filepath}.lua");
+        });
         // 为每个脚本设置一个独立的环境，可一定程度上防止脚本间全局变量、函数冲突
         LuaTable meta = luaEnv.NewTable();
         meta.Set("__index", luaEnv.Global);
@@ -35,7 +36,8 @@ public class LuaState : MonoBehaviour
         //     scriptEnv.Set(injection.name, injection.value);
         // }
 
-        luaEnv.DoString(luaScript.GetDecodeBytes(), "LuaTestScript", scriptEnv);
+        // luaEnv.DoString(AddressablesUtility.GetBytesFromAssetReference(_addressableTextAsset), "LuaTestScript", scriptEnv);
+        luaEnv.DoString(AddressablesUtility.GetBytesFromAssetPath("Assets/res/scripts/hello.lua"), "LuaTestScript", scriptEnv);
 
         Action luaAwake = scriptEnv.Get<Action>("awake");
         scriptEnv.Get("start", out luaStart);
