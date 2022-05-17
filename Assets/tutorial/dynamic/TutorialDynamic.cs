32a77492e91312240880a65a1b30995850c98f78
thz <lixuepeng@bilibili.com>
2022-05-18 01:04:08 +0800

    [反射 dynamic的使用]

---
+++ Assets/tutorial/dynamic/TutorialDynamic.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialDynamicTest
+{
+    public int a;
+    public string b;
+    public void test(bool c)
+    {
+        Debug.Log($"{a}.{b}.{c}");
+    }
+}
+public class TutorialDynamic : MonoBehaviour
+{
+    // Start is called before the first frame update
+    async void Start()
+    {
+        var type = System.Type.GetType("TutorialDynamicTest");
+        dynamic t = System.Activator.CreateInstance(type);
+        t.a = 1;
+        t.b = "test";
+        t.test(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
