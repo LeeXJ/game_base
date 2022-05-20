9574ef80055cfdc1c7fd23ddebef0fb3973b2771
thz <lixuepeng@bilibili.com>
2022-05-21 00:20:49 +0800

    [扩容不拷贝数组尝试]

---
+++ Assets/src/core/SmartList.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmartList<T>
+{
+    private List<List<T>> datas = new List<List<T>>(1);
+    private int size;
+    private int used = 0;
+    private int capacity;
+
+    public SmartList(int size = 8)
+    {
+        this.size = size;
+        capacity += size;
+        datas.Add(new List<T>(size));
+    }
+
+    public void Add(T item)
+    {
+        int data = used / 8;
+        datas[data].Add(item);
+        used++;
+        // 扩容
+        if (used >= capacity)
+        {
+            capacity += size;
+            datas.Add(new List<T>(size));
+        }
+    }
+
+    public T this[int i]
+    {
+        get
+        {
+            int data = i / 8;
+            int index = i % 8;
+            return datas[data][index];
+        }
+        set
+        {
+            int data = i / 8;
+            int index = i % 8;
+            datas[data][index] = value;
+        }
+    }
+
+    public int Count
+    {
+        get
+        {
+            return used;
+        }
+    }
+
+}
---
+++ Assets/tutorial/smartlist/tutorialSmartList.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tutorialSmartList : MonoBehaviour
+{
+    // Start is called before the first frame update
+    async void Start()
+    {
+        SmartList<int> list = new SmartList<int>();
+        list.Add(1);
+        list.Add(2);
+        list.Add(3);
+        list.Add(4);
+        list.Add(5);
+        list.Add(6);
+        list.Add(7);
+        list.Add(8);
+        list.Add(9);
+        for (int i = 0; i < list.Count; ++i)
+        {
+            Debug.Log(list[i]);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
