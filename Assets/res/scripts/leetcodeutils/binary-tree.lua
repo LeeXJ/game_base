7c5e8ce3011f4340de8df515ef000a55f1132e4e
thz <lixuepeng@bilibili.com>
2022-05-28 00:02:06 +0800

    [101. 对称二叉树]

--- Assets/res/scripts/leetcodeutils/binary-tree.lua
+++ Assets/res/scripts/leetcodeutils/binary-tree.lua
@@ -1,29 +1,52 @@
 local list = nil
 local i = 0
+local j = 0
+local queue = nil
 
-local function pop()
+local function next()
     i = i + 1
     return list[i]
 end
 
-local function createNode(val)
-    if nil ~= val then
-        local node = {}
-        node.val = val
-        local left = pop()
-        local right = pop()
+local function pop()
+    j = j + 1
+    return queue[j]
+end
+
+local function push(val)
+    queue[#queue+1] = val
+end
+
+local function empty()
+    return j >= #queue
+end
+
+local function createNode()
+    while not empty() do
+        local node = pop()
+        local left = next()
+        local right = next()
         if left then
-            node.left = createNode(left)
+            node.left = {}
+            node.left.val = left
+            push(node.left)
         end
         if right then
-            node.right = createNode(right)
+            node.right = {}
+            node.right.val = right
+            push(node.right)
         end
-        return node
     end
 end
 
 function BuildBinaryTree(data)
     list = data
     i = 0
-    return createNode(pop())
+    queue = {}
+    local root = {}
+    root.val = next()
+    push(root)
+    j = 0
+    createNode()
+    return root
 end
